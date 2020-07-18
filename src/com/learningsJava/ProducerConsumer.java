d32ab46fa72c8f19df8fa8a9f89a4a31b2c5681c
Ankur Sahoo <shanky442@gmail.com>
2020-07-18 13:28:51 +0100

    used synchronized block

--- src/com/learningsJava/ProducerConsumer.java
+++ src/com/learningsJava/ProducerConsumer.java
@@ -28,34 +28,39 @@ public class ProducerConsumer {
     public static class PC {
         List<Integer> list = new LinkedList<>();
         int initialCapacity = 1;
-        public synchronized void produce() throws InterruptedException {
+        public void produce() throws InterruptedException {
             int value=10;
             while(true) {
-                if(list.size()<initialCapacity) {
-                    value++;
-                    list.add(value);
-                    Thread.sleep(2000);
-                    System.out.println("producer produced"+value);
-                } else {
-                    this.notify();
-                    this.wait();
+                synchronized(this) {
+                    if(list.size()<initialCapacity) {
+                        value++;
+                        list.add(value);
+                        Thread.sleep(1000);
+                        System.out.println("producer produced "+value);
+                    } else {
+                        this.notify();
+                        //this.wait();
+                    }
                 }
+
             }
         }
 
-        public synchronized void consume() throws InterruptedException {
+        public void consume() throws InterruptedException {
             System.out.println("inside consumer");
-            this.wait(); // so that consumer starts only when it is notified by producer
+            //this.wait(); // so that consumer starts only when it is notified by producer
             /*for(int i:list) {
                 System.out.println("elements in list are "+i);
             }*/
             while(true) {
-                if(!list.isEmpty()) {
-                    System.out.println("consumer consumed"+list.remove(0));
-                    Thread.sleep(2000); //so that we can see the output
-                } else  {
-                    this.notify();
-                    this.wait();
+                synchronized (this) {
+                    if (!list.isEmpty()) {
+                        System.out.println("consumer consumed " + list.remove(0));
+                        Thread.sleep(1000); //so that we can see the output
+                    } else {
+                        //this.notify();
+                        this.wait();
+                    }
                 }
             }
         }
